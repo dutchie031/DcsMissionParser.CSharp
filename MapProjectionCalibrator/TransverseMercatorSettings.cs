9a4da2fb64672b4f1e16ede21e40ed736d74c5a4
dutchie032 <dutchie032>
2026-01-02 17:47:25 +0100

    wip

---
+++ MapProjectionCalibrator/TransverseMercatorSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapProjectionCalibrator
+{
+    internal class TransverseMercatorSettings
+    {
+        public double CentralMeridian { get; set; }
+        public double ScaleFactor { get; set; }
+        public double FalseEasting { get; set; }
+        public double FalseNorthing { get; set; }
+    }
+}

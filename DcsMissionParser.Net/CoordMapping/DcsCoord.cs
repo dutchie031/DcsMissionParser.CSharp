ba937691c5fc4d44db0d3ce3a5dabc965876d09c
dutchie031 <timrorije@gmail.com>
2026-06-01 21:58:33 +0200

    Added constors to coords and latlong

--- DcsMissionParser.Net/CoordMapping/DcsCoord.cs
+++ DcsMissionParser.Net/CoordMapping/DcsCoord.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DcsMissionParser.Net.CoordMapping;
 
 public class DcsCoord
 {
-    public double X { get; set; }
-    public double Y { get; set; }
+    public DcsCoord(){}
+
+    [SetsRequiredMembers]
+    public DcsCoord(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public required double X { get; set; }
+    public required double Y { get; set; }
 }
--- DcsMissionParser.Net/CoordMapping/LatLong.cs
+++ DcsMissionParser.Net/CoordMapping/LatLong.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DcsMissionParser.Net.CoordMapping;
 
 public class LatLong
 {
-    public double Lat { get; set; }
-    public double Lon { get; set; }
+    public LatLong(){}
+    
+    [SetsRequiredMembers]
+    public LatLong(double lat, double lon)
+    {        
+        Lat = lat;
+        Lon = lon;
+    }
+
+    public required double Lat { get; set; }
+    public required double Lon { get; set; }
 }
